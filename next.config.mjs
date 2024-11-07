52ecb3c6c05ddea64a92a371f9ab2ada9c4d20fc
Ashwin Gorle <ashwingorle7777@gmail.com>
2024-11-07 23:45:05 +0530

    after removing eslint

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,10 @@ const nextConfig = {
     images:{
         domains:['cdn-icons-png.flaticon.com'],
         unoptimized:true
-    }
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+      },
 };
 
 export default nextConfig;
