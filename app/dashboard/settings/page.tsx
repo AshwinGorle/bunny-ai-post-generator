9f0dd37028fe1d23ada2095496aced5f77df1831
Bhishek-Parmar <itsbhishek@gmail.com>
2024-11-14 18:38:05 +0530

    change base theme to dark for clerk

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,10 +1,13 @@
 import { UserProfile } from '@clerk/nextjs'
 import React from 'react'
 
+// @ts-ignore
+import { dark } from '@clerk/themes'
+
 function Settings() {
   return (
     <div className='flex items-center justify-center h-full'>
-        <UserProfile/>
+        <UserProfile appearance={{baseTheme : dark}}   routing="hash" />
     </div>
   )
 }
