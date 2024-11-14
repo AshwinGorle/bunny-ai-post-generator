5863e7875049aeac163c7e8df6e964b7ee7441cc
Bhishek-Parmar <itsbhishek@gmail.com>
2024-11-14 18:51:51 +0530

    change theme of sign in & sign up

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import {dark} from "@clerk/themes";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen bg-black">
          <SignIn appearance={{baseTheme: dark}}  routing="hash" />
     </div>
  
